35a5525745f89c3c0ebb85883ddb10e4abc90da3
Aidan <dev@aidanlowson.com>
2022-07-06 21:41:28 +0100

    Update fixed.handler.js
    Just a couple of changes to get it up and running in Lambda nodev16
    
    url -> uri and exports.handler

--- next-js-lambda-routing-proxy/functions/fixed.handler.js
+++ next-js-lambda-routing-proxy/functions/fixed.handler.js
@@ -1,12 +1,12 @@
 const hasExtension = /(.+)\.[a-zA-Z0-9]{2,5}$/;
 const hasSlash = /\/$/;
 
-export async function handler(event, _, callback) {
+exports.handler = (event, _, callback) => {
   const { request } = event.Records[0].cf;
   const uri = request.uri;
 
-  if (uri && !uri.match(hasExtension) && !url.match(hasSlash)) {
+  if (uri && !uri.match(hasExtension) && !uri.match(hasSlash)) {
     request.uri = `${uri}.html`;
   }
   return callback(null, request);
-}
+};
